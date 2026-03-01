aec34bc2b25becd2e79b066ac72dbcc402d6c776
MayeCamargo <est.diana.camargo3@unimilitar.edu.co>
2026-03-01 11:36:10 -0500

    carga de cambios 11:36 am

--- c_javascript/1_scripts_del_home.js
+++ c_javascript/1_scripts_del_home.js
@@ -1,18 +1,26 @@
-// corregir sintaxis y agregar funcionalidad de redirección
-function metodoofuncionsaludar() {
-    const contenedordelsaludoenjava = document.getElementById("contenedordelsaludodesdehtml");
-    if (contenedordelsaludoenjava) {
-        contenedordelsaludoenjava.textContent = "Hola, esto fue impreso desde javascript";
-    }
-}
-
 // navegación al hacer clic en el icono de login
 document.addEventListener('DOMContentLoaded', () => {
     const loginIcon = document.getElementById('ContenedorIconoLogin');
     if (loginIcon) {
         loginIcon.addEventListener('click', () => {
             // cambiar la ruta según el nombre real de la página
-            window.location.href = '../a_html/2_index_del_login.html';
+			window.location.href = '../a_html/2_index_del_login.html';
+        });
+    }
+
+    const whatsappIcon = document.getElementById('ContenedorIconoWhatsapp');
+    if (whatsappIcon) {
+        whatsappIcon.addEventListener('click', () => {
+            // ir a la página de whatsapp simulada
+            window.location.href = '../a_html/3_index_del_whatsapp.html';
+        });
+    }
+
+    const instaIcon = document.getElementById('ContenedorIconoInstagram');
+    if (instaIcon) {
+        instaIcon.addEventListener('click', () => {
+            // abrir Instagram externo
+            window.location.href = 'https://www.instagram.com/maye_rap_2026/';
         });
     }
 });
--- c_javascript/2_scripts_del_login.js
+++ c_javascript/2_scripts_del_login.js
@@ -8,4 +8,22 @@ document.addEventListener('DOMContentLoaded', () => {
             window.location.href = '../index.html';
         });
     }
+
+    const whatsappIcon = document.getElementById('ContenedorIconoWhatsapp');
+    if (whatsappIcon) {
+        whatsappIcon.addEventListener('click', () => {
+            // ir a la página de whatsapp simulada
+            window.location.href = '../a_html/3_index_del_whatsapp.html';
+        });
+    }
+
+    const instaIcon = document.getElementById('ContenedorIconoInstagram');
+    if (instaIcon) {
+        instaIcon.addEventListener('click', () => {
+            // abrir Instagram externo
+            window.location.href = 'https://www.instagram.com/maye_rap_2026/';
+        });
+    }
+
 });
+
